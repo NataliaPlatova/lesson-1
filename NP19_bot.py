e0f997cbfc04ee79679483450efeeba890d0496d
Natalia <n.platova@hotmail.com>
2019-05-30 11:49:56 +0700

    7

--- NP19_bot.py
+++ NP19_bot.py
@@ -1,5 +1,6 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+import ephem
 import logging
 
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
@@ -17,6 +18,8 @@ def main():
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
+    # dp.add_handler(CommandHandler("planet", "planet_name", say_constellation ))
+
     mybot.start_polling()
     mybot.idle()
 
@@ -30,5 +33,10 @@ def talk_to_me(bot, update):
     user_text = update.message.text
     print(user_text)
     update.message.reply_text(user_text)
+
+#def say_constellation(bot, update):
+ #   user_text = update.message.text.split()
+  #  if ephem.constellation
+   #     update.message.reply_text(ephem.constellation(user_text))
 
 main()
